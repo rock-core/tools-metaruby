2690eee3bd4af69ff70312ffca0d9d893e3e1bac
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-02-14 21:27:30 +0100

    reinstate using a separate #apply_block in ModelAsClass#setup_submodel
    Otherwise, subclasses cannot define their own evaluation contexts,
    as e.g. what syskit does to allow setting the task interface

--- lib/metaruby/class.rb
+++ lib/metaruby/class.rb
@@ -68,6 +68,10 @@ module MetaRuby
             model
         end
 
+        def apply_block(&block)
+            class_eval(&block)
+        end
+
         # Called at the end of the definition of a new submodel
         def setup_submodel(submodel, &block)
             register_submodel(submodel)
@@ -75,7 +79,7 @@ module MetaRuby
             # Note: we do not have to call #register_submodel manually here,
             # The inherited hook does that for us
             if block_given?
-                submodel.class_eval(&block)
+                submodel.apply_block(&block)
             end
         end
 
