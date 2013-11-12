4a167386f8a97d0dcc8f183a79f72f0e411ecd34
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-11-12 17:30:47 +0100

    dsls: implement find_throug_method_missing

--- lib/metaruby/dsls/find_through_method_missing.rb
+++ lib/metaruby/dsls/find_through_method_missing.rb
@@ -1,5 +1,48 @@
 module MetaRuby
     module DSLs
+        # Generic implementation to create suffixed accessors for child objects
+        # on a class
+        #
+        # Given an object category (let's say 'state'), this allows to properly
+        # implement a method-missing based accessor of the style
+        #
+        #     blabla_state
+        #
+        # using a find_state method that the object should respond to
+        #
+        # @param [Object] object the object on which the find method is going to
+        #   be called
+        # @param [Symbol] m the method name
+        # @param [Array] args the method arguments
+        # @param [Array<String>] suffixes the accessor suffixes that should be
+        #   resolved
+        # @return [Object,nil] an object if one of the listed suffixes matches
+        #   the method name, or nil if the method name does not match the
+        #   requested pattern.
+        #
+        # @raise [NoMethodError] if the requested object does not exist (i.e. if
+        #   the find method returns nil)
+        # @raise [ArgumentError] if the method name matches one of the suffixes,
+        #   but arguments were given
+        #
+        #
+        # @example
+        #   class MyClass
+        #     def find_state(name)
+        #       states[name]
+        #     end
+        #     def find_transition(name)
+        #       transitions[name]
+        #     end
+        #     def method_missing(m, *args, &block)
+        #       MetaRuby::DSLs.find_through_method_missing(self, m, *args,
+        #         'state', 'transition') || super
+        #     end
+        #   end
+        #   object = MyClass.new
+        #   object.add_state 'my'
+        #   object.my_state # will resolve the 'my' state
+        #
         def self.find_through_method_missing(object, m, args, *suffixes)
             m = m.to_s
             suffixes.each do |s|
