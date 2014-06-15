c780261cc7f7a1ae4df2f5ebe916db0cf8bf9728
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-06-15 15:25:35 +0200

    test: update common setup file
    This adds debugger integration for minitest, for both 1.9 and 2.0
    if TEST_DEBUG is set to 1. It makes minitest drop a pry session when
    a failure or unhandled exception is thrown.

--- lib/metaruby/test.rb
+++ lib/metaruby/test.rb
@@ -13,27 +13,23 @@ if ENV['TEST_ENABLE_COVERAGE'] == '1'
     end
 end
 
-require 'minitest/autorun'
 require 'metaruby'
-require 'flexmock/test_unit'
+require 'minitest/autorun'
 require 'minitest/spec'
+require 'flexmock/test_unit'
 
 if ENV['TEST_ENABLE_PRY'] != '0'
     begin
         require 'pry'
+        if ENV['TEST_DEBUG'] == '1'
+            require 'pry-rescue/minitest'
+        end
     rescue Exception
         MetaRuby.warn "debugging is disabled because the 'pry' gem cannot be loaded"
     end
 end
 
 module MetaRuby
-    if defined? FlexMock
-        class MiniTest::Test
-            include FlexMock::ArgumentTypes
-            include FlexMock::MockContainer
-        end
-    end
-
     # This module is the common setup for all tests
     #
     # It should be included in the toplevel describe blocks
@@ -63,4 +59,21 @@ module MetaRuby
     end
 end
 
+# Workaround a problem with flexmock and minitest not being compatible with each
+# other (currently). See github.com/jimweirich/flexmock/issues/15.
+if defined?(FlexMock) && !FlexMock::TestUnitFrameworkAdapter.method_defined?(:assertions)
+    class FlexMock::TestUnitFrameworkAdapter
+        attr_accessor :assertions
+    end
+    FlexMock.framework_adapter.assertions = 0
+end
+
+module Minitest
+    class Spec
+        include MetaRuby::SelfTest
+    end
+    class Test
+        include MetaRuby::SelfTest
+    end
+end
 
